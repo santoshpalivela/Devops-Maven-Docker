cb6747a25f7a1c93ba339007c1cd5acb4ebc864f
root <root@ip-172-31-7-29.ap-south-1.compute.internal>
2022-12-18 06:37:59 +0000

    modified src

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -40,7 +40,7 @@ public class LoginServlet extends HttpServlet {
 		out.println("<title>Techlanders Solutions</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1> Hi , This is Maz yafai from Devops </h1>");
+		out.println("<h1> Hi , This is synechron training </h1>");
 		out.println("</body>");
 		out.println("</html>");
 
